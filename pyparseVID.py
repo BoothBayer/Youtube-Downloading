f233a2e9261e74bb897873f0bc0b8ddea0b552f8
BoothBayer <BoothBayer@users.noreply.github.com>
2019-07-26 13:05:51 -0400

    minor changes

--- pyparseVID.py
+++ pyparseVID.py
@@ -4,13 +4,16 @@ import datetime
 from pytube import YouTube
 import os
 
+def comp_func(stream,file_handle):
+	print("Downloaded: ", file_handle, " " ,stream)
+
 def downloadYouTube(videourl, path, cnt):
 	try:
 		yt = YouTube(videourl)
-		yt2 = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 		if not os.path.exists(path):
 			os.makedirs(path)
 		print("Downloading " + yt.title)
+		yt2 = yt.streams.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first()
 		yt2.download(output_path=path)
 		print('Downloaded {} | {}'.format(cnt, datetime.datetime.now()))
 	except Exception as e:
@@ -23,7 +26,9 @@ with open(filepath) as fp:
 	while line:
 		print("Line {}: {}".format(cnt, line.strip()))
 		downloadYouTube('{}'.format(line.strip()), './Directory', cnt)
+		print("Done Download")
 		line = fp.readline()
 		cnt += 1
+		print("Count increased")
 print("quitting")
-sys.exit()
+exit()
