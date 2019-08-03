04bf3580cac81c6f00da147603c5ba4918d2310f
BoothBayer <BoothBayer@users.noreply.github.com>
2019-08-02 21:18:31 -0400

    Added progress tracker and threading

--- pyparseVID.py
+++ pyparseVID.py
@@ -1,24 +1,67 @@
 from pytube import YouTube
 import time
 import datetime
-from pytube import YouTube
+import sys
 import os
-
-def comp_func(stream,file_handle):
-	print("Downloaded: ", file_handle, " " ,stream)
-
+from threading import Thread
+threads = []
+def percent(tem, total):
+        x = float(total - tem)
+        perc = (float(x) / float(total)) * float(100)
+        return perc
+a = 0 #used to stop percents from reposting
+v = 0 #^
+def progress_callback(stream,chunk,file_handle,bytes_remaining):
+        global a
+	global v
+        size = stream.filesize
+        y = round(percent(bytes_remaining,size),0)
+	try:
+		if int(y) != 0:
+			if (100%(int(y)) == 0):
+				if stream.includes_audio_track:
+					if a != y:
+						print('AUD: '+str(y)+'%')
+						a = y
+				elif stream.includes_video_track:
+					if v != y:
+						print('VID: '+str(y)+'%')
+						v = y
+	except Exception as e:
+		print(e)
 def downloadYouTube(videourl, path, cnt):
 	try:
 		yt = YouTube(videourl)
+		yt.register_on_progress_callback(progress_callback)
 		if not os.path.exists(path):
 			os.makedirs(path)
-		print("Downloading " + yt.title)
-		yt2 = yt.streams.filter(progressive=True,file_extension='mp4').order_by('resolution').desc().first()
-		yt2.download(output_path=path)
+		print(yt.title)
+#		print(yt.streams.filter(adaptive=True).all()) #.order_by('resolution').desc().first()
+		print("get 299vid")
+		yt2 = yt.streams.get_by_itag('299') #itag299 is hd vid and 140 is matching audio
+		processV = Thread(target=download, args=[yt2,path,'VID'])
+		processV.start()
+		threads.append(processV)
+		print("get 140aud")
+		yt2 = yt.streams.get_by_itag('140')
+		processA = Thread(target=download, args=[yt2,path,'AUD'])
+		processA.start()
+		threads.append(processA)
+		for process in threads:
+			process.join()
 		print('Downloaded {} | {}'.format(cnt, datetime.datetime.now()))
 	except Exception as e:
 		print("ERROR: ", e)
-
+def download(stream,path,h):
+	print('Downloading: ' + str(stream))
+	try:
+		if stream.includes_audio_track:
+			print("includes audio", h)
+		elif stream.includes_video_track:
+			print("includes video", h)
+		stream.download(output_path=path,filename_prefix=h)
+	except Exception as e:
+		print(stream," ERROR DL: ", e)
 filepath = 'playlistLinks'
 with open(filepath) as fp:
 	line = fp.readline()
@@ -26,9 +69,8 @@ with open(filepath) as fp:
 	while line:
 		print("Line {}: {}".format(cnt, line.strip()))
 		downloadYouTube('{}'.format(line.strip()), './Directory', cnt)
-		print("Done Download")
+		print("Done Downloading")
 		line = fp.readline()
 		cnt += 1
-		print("Count increased")
 print("quitting")
 exit()
