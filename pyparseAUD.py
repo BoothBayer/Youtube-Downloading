70caaeac275450ad9e7308c879feee9710d78fe3
BoothBayer <BoothBayer@users.noreply.github.com>
2019-08-02 21:21:16 -0400

    Added Threading

--- pyparseAUD.py
+++ pyparseAUD.py
@@ -2,28 +2,41 @@ from pytube import YouTube
 import time
 import datetime
 import os
-import subprocess
-import sys
-def downloadYouTube(videourl, path, cnt):
+from threading import Thread
+x= 0
+ytDlL = []
+threads = []
+def downloadYouTube(yt, path):
 	try:
-		yt = YouTube(videourl)
-		yt2 = yt.streams.filter(only_audio=True).first()
 		if not os.path.exists(path):
 			os.makedirs(path)
 		print("Downloading " + yt.title)
-		yt2.download(output_path=path)
-		print('Downloaded {} | {}'.format(cnt, datetime.datetime.now()))
+		yt2 = yt.streams.filter(only_audio=True).first()
+		yt2.download(output_path=path,filename_prefix="(AudioOnly)")
+		print('Downloaded {} | {}'.format(yt.title, datetime.datetime.now()))
 	except Exception as e:
 		print("ERROR: ", e)
 
 filepath = 'playlistLinks'
-with open(filepath) as fp:  
+with open(filepath) as fp:
 	line = fp.readline()
 	cnt = 1
 	while line:
 		print("Line {}: {}".format(cnt, line.strip()))
-		downloadYouTube('{}'.format(line.strip()), './Directory', cnt)
+		yt = YouTube(line.strip())
+		ytDlL.append(yt)
+		print("Added {} to ytDlL".format(yt))
+		#downloadYouTube('{}'.format(line.strip()), './Directory', cnt)
+		#print("Done Download")
 		line = fp.readline()
 		cnt += 1
+for yt in ytDlL:
+	process = Thread(target=downloadYouTube,args=[yt, './Directory'])
+	print("Starting process {} | {}".format(x, yt.title))
+	process.start()
+	threads.append(process)
+	x += 1
+for process in threads:
+	process.join()
 print("quitting")
-sys.exit()
+exit()
